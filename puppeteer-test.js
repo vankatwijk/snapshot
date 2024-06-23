41a8f8565a2b7016cd0c3c51dd77f575a7a7dd8c
Hendrikus  van Katwijk <hendrikus.hpvk@gmail.com>
2024-06-23 17:04:54 +0200

    Update puppeteer-test.js

--- puppeteer-test.js
+++ puppeteer-test.js
@@ -1,10 +1,15 @@
-const puppeteer = require('puppeteer'); (async () => { try { const browser = await 
-        puppeteer.launch({
-            headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']
+const puppeteer = require('puppeteer');
+
+(async () => {
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
-        const page = await browser.newPage(); await 
-        page.goto('https://www.google.com'); console.log('Page title:', await 
-        page.title()); await browser.close();
+        const page = await browser.newPage();
+        await page.goto('https://www.google.com');
+        console.log('Page title:', await page.title());
+        await browser.close();
     } catch (error) {
         console.error('Error launching browser:', error);
     }
