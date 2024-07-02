09e1ed7c8e92f7ad3f6e34bf013740ac9c0fdd64
Hendrikus  van Katwijk <hendrikus.hpvk@gmail.com>
2024-07-02 22:22:42 +0200

    Create launchBrowser.js

---
+++ launchBrowser.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+const maxRetries = 5;
+
+async function launchBrowser(retries = 0) {
+    try {
+        return await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            ignoreHTTPSErrors: true
+        });
+    } catch (error) {
+        if (retries < maxRetries) {
+            console.error(`Error launching browser, retrying... (${retries + 1}/${maxRetries})`);
+            return await launchBrowser(retries + 1);
+        } else {
+            throw new Error('Failed to launch browser after multiple retries');
+        }
+    }
+}
+
+module.exports = launchBrowser;
