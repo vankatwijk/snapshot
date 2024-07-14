07dd0163e6348a09070051836c091326f85846a6
Hendrikus  van Katwijk <hendrikus.hpvk@gmail.com>
2024-07-14 21:56:22 +0200

    Update screenshotWorker.js

--- screenshotWorker.js
+++ screenshotWorker.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const path = require('path');
 const sharp = require('sharp');
 
 process.on('message', async ({ url, device, cacheFile }) => {
